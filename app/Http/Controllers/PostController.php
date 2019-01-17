fbc8ef9d84dd40fd036a003c481d081c1090e4a6
SHAJ <shakilahmedshaj@gmail.com>
2019-01-17 23:37:05 +0600

    Access Control

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,16 @@ use DB;
 class PostController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -91,6 +101,11 @@ class PostController extends Controller
     {
         $post = Post::find($id);
 
+        // Check for correct user
+        if(auth()->user()->id !==$post->user_id){
+            return redirect('/posts')->with('error', 'Unauthorized Page');
+        }
+
 
         return view('posts.edit')->with('post', $post);
     }
@@ -132,6 +147,11 @@ class PostController extends Controller
     public function destroy($id)
     {
         $post = Post::find($id);
+
+        // Check for correct user
+        if(auth()->user()->id !==$post->user_id){
+            return redirect('/posts')->with('error', 'Unauthorized Page');
+        }
         
         $post->delete();
         return redirect('/posts')->with('success', 'Post Removed');
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -15,12 +15,17 @@
     </div>
   </div>
   <hr>
-  <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
 
-  {!!Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+  @if(!Auth::guest())
+        @if(Auth::user()->id == $post->user_id)
+            <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+
+            {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
                 {{Form::hidden('_method', 'DELETE')}}
                 {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-  {!!Form::close()!!}
+            {!!Form::close()!!}
+        @endif
+    @endif
   
 
 @endsection
